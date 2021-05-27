ebfcddf63511cb372b2ca9af7162e289c5256df1
Akhil <akhilf007@gmail.com>
2021-05-28 00:53:29 +0530

    select shoe size and add to cart

---
+++ checkout.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+
+import time
+
+PATH = './chromedriver'
+
+driver = webdriver.Chrome (PATH)
+
+print ('opening webpage ... ')
+
+driver.get ('https://www.nike.com/in/t/zion-1-pf-basketball-shoe-bHGvxg/DA3129-001')
+
+# wait 10 seconds for page to load
+time.sleep (10)
+
+print ("Searching for Size selector ... ")
+shoeSize = driver.find_elements_by_class_name ("css-xf3ahq")
+print ("Number of elements found:", len (shoeSize))
+shoeSize[4].click ()
+
+addToBag = driver.find_element_by_class_name ("add-to-cart-btn")
+addToBag.click ()
+
+# wait 5 seconds after clicking the button
+time.sleep (5)
+
+driver.get ('https://www.nike.com/cart')
+
+
+time.sleep (5)
